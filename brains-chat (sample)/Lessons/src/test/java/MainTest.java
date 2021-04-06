5e5be3d2c5868088aaa3185177aa6056f99ca893
ShamsutOFF <shamsutoff@yandex.ru>
2021-04-06 15:46:48 +0300

    Create HW06 without log files in chat

---
+++ brains-chat (sample)/Lessons/src/test/java/MainTest.java
@@ -0,0 +1,70 @@
+package test.java;
+
+import Lesson06.Main;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static Lesson06.Main.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+
+    @Test
+    void method011() {
+        int testArr[] = {1, 2, 3, 4, 5, 6, 7};
+        int expArr[] = {5, 6, 7};
+        Assertions.assertArrayEquals (expArr, method01 (testArr), "ОК");
+    }
+
+    @Test
+    void method012() {
+        int testArr[] = {1, 2, 3, 4, 5, 6, 4, 1, 2};
+        int expArr[] = {1, 2};
+        Assertions.assertArrayEquals (expArr, method01 (testArr), "ОК");
+    }
+
+    @Test
+    void method01RuntimeException() {
+        int testArr[] = {1, 2, 3, 5, 6, 1, 2};
+        Assertions.assertThrows (RuntimeException.class, () -> method01 (testArr));
+    }
+
+    @Test
+    void method01RuntimeException2() {
+        int testArr[] = {};
+        Assertions.assertThrows (RuntimeException.class, () -> method01 (testArr));
+    }
+
+    @Test
+    void method021() {
+        int testArr[] = {1, 2, 3, 4, 5, 6, 4, 1, 2};
+        Assertions.assertTrue (method02 (testArr));
+    }
+
+    @Test
+    void method022() {
+        int testArr[] = {2, 2, 3, 4, 5, 6, 4, 0, 2};
+        Assertions.assertTrue (method02 (testArr));
+    }
+
+    @Test
+    void method023() {
+        int testArr[] = {2, 2, 3, 1, 5, 6, 0, 2};
+        Assertions.assertTrue (method02 (testArr));
+    }
+
+    @Test
+    void method024() {
+        int testArr[] = {2, 2, 3, 5, 6, 0, 2};
+        Assertions.assertFalse (method02 (testArr));
+    }
+
+    @Test
+    void method025() {
+        int testArr[] = {};
+        Assertions.assertFalse (method02 (testArr));
+    }
+}
